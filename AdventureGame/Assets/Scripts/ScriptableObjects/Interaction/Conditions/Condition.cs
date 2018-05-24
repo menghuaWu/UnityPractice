0ee1a252a90d82e02eb6e382d28175d3749baa5b
Scott.Wu <wumenghua0429@gmail.com>
2018-05-24 14:15:56 +0800

    20180524

--- AdventureGame/Assets/Scripts/ScriptableObjects/Interaction/Conditions/Condition.cs
+++ AdventureGame/Assets/Scripts/ScriptableObjects/Interaction/Conditions/Condition.cs
@@ -2,6 +2,7 @@
 
 public class Condition : ScriptableObject
 {
+    //條件容器，儲存條件敘述，以及條件是否成立
     public string description;
     public bool satisfied;
     public int hash;
--- AdventureGame/Assets/Scripts/ScriptableObjects/Interaction/Conditions/ConditionCollection.cs
+++ AdventureGame/Assets/Scripts/ScriptableObjects/Interaction/Conditions/ConditionCollection.cs
@@ -2,22 +2,25 @@
 
 public class ConditionCollection : ScriptableObject
 {
-    public string description;
-    public Condition[] requiredConditions = new Condition[0];
-    public ReactionCollection reactionCollection;
 
+    public string description;//描述
+    public Condition[] requiredConditions = new Condition[0];//所需要的條件
+    public ReactionCollection reactionCollection;//會有甚麼反映
 
     public bool CheckAndReact()
     {
         for (int i = 0; i < requiredConditions.Length; i++)
         {
-            if (!AllConditions.CheckCondition (requiredConditions[i]))
+            if (!AllConditions.CheckCondition(requiredConditions[i]))
+            {
                 return false;
+            }
+
+            if (reactionCollection)
+            {
+                reactionCollection.React();
+            }
         }
-
-        if(reactionCollection)
-            reactionCollection.React();
-
         return true;
     }
 }
